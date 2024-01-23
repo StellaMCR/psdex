dae878ccc522c6bb996637794a79dbe5bca042a0
StellaResende <stellamcruzr@gmail.com>
2024-01-23 13:39:37 -0300

    add styles

--- src/Details/detailsStyles.ts
+++ src/Details/detailsStyles.ts
@@ -3,3 +3,20 @@ export const detailsContentStyle: React.CSSProperties = {
     flexDirection: 'column',
     width: '100vw',
 };
+
+export const imageCardStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'coral'
+};
+
+
+
+export const spaceMobileStyle: React.CSSProperties = {
+    width: '100%',
+    alignItems: "center"
+};
+export const spaceDesktopStyle: React.CSSProperties = {
+    width: '100%',
+};
